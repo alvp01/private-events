3229c87b2ebce2a13a09cc534b44071a6ce6cf78
Paul Balitema Kabali <engineerbpk@gmail.com>
2020-10-14 01:07:25 +0300

    Sign in user after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,8 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        session[:user_name] = @user.username
+        session[:user_id] = @user.id
         format.html { redirect_to user_path(@user.id), notice: 'User was successfully created.' }
       else
         format.html { render :new }
