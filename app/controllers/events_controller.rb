68df2d99a2ff7d5c2704264c72d211bd4d367797
Abel <abel.lavieri@gmail.com>
2020-10-13 13:45:14 -0400

    Add past and upcoming methods for Event class and index view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,8 @@
 class EventsController < ApplicationController
 
   def index
-    @events = Event.all
+    @past_events = Event.past
+    @upcoming_events = Event.upcoming
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,14 @@
+require 'date'
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :attendances, foreign_key: "attended_event_id"
   has_many :attendees, through: :attendances
+
+  def self.past
+    Event.where("event_date < ?", Date.current).all
+  end
+
+  def self.upcoming
+    Event.where("event_date >= ?", Date.current).all
+  end
 end
